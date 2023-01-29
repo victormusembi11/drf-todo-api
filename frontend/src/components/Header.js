b4dd89ef6d2191f802d8de3365278f6793d64f24
vic m <vm@vics-MacBook-Pro.local>
2023-01-29 18:46:16 +0300

    feat: add task button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,14 @@
+import Button from "./Button"
+
 const Header = (props) => {
+  const onClick = () => {
+    console.log('Click');
+  }
+
   return (
     <header>
         <h1>{props.title}</h1>
+        <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }

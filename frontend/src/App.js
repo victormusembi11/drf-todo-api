8d67d61ca2b79d72709220561b2f3359f2b18ac7
vic m <vm@vics-MacBook-Pro.local>
2023-01-29 19:24:03 +0300

    feat: display tasks
    created tasks and task components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTask] = useState(
+    [
+        {
+            id: 1,
+            title: 'Meal prep',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'Sleep',
+            completed: false
+        }
+    ]
+  )
+
   return (
     <div className="App">
       <Header title="Todo App" />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
---
+++ frontend/src/components/Task.js
@@ -0,0 +1,9 @@
+const Task = ({ task }) => {
+  return (
+    <div className="task">
+      <h3>{task.title}</h3>
+    </div>
+  )
+}
+
+export default Task

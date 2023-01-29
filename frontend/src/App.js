972dc1f9a0dda3db01c1e4ec00f96e3941947412
vic m <vm@vics-MacBook-Pro.local>
2023-01-29 18:29:42 +0300

    feat: header component with title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello from React</h1> 
+      <Header title="Todo App" />
     </div>
   );
 }

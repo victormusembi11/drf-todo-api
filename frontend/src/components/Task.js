397937de869b619fa46fcf109f0255ad8aade5c3
vic m <vm@vics-MacBook-Pro.local>
2023-01-29 19:28:51 +0300

    feat: added delete icons for tasks

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,7 +1,9 @@
+import { FaTimes } from "react-icons/fa"
+
 const Task = ({ task }) => {
   return (
     <div className="task">
-      <h3>{task.title}</h3>
+      <h3>{task.title} <FaTimes style={{ color: 'red', cursor: 'pointer' }}/></h3>
     </div>
   )
 }

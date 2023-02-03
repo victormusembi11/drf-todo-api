388069ac570ee2668b8bce4819bc6c8d1213e866
vic m <vm@vics-MacBook-Pro.local>
2023-02-03 21:14:45 +0300

    feat: jwt authenication

--- config/settings/base.py
+++ config/settings/base.py
@@ -47,8 +47,10 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
 
     'todo',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://127.0.0.1:3000",
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
